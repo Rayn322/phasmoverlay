5c2af0e34fe4fb6bc2800656c8294cd292312ed2
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2024-06-16 20:51:29 -0700

    Set size with padding

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{Manager, PhysicalSize, Size};
+use tauri::{Manager, PhysicalSize};
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let window = app.get_webview_window("main").unwrap();
 
-            window.set_size(PhysicalSize::new(1920, 1080));
-            window.center();
+            // get size and add padding
+            let binding = window.current_monitor().unwrap().unwrap();
+            let monitor_size = binding.size();
+            let size = PhysicalSize {
+                width: (monitor_size.width as f64 * 0.75).round(),
+                height: (monitor_size.height as f64 * 0.75).round(),
+            };
+
+            window.set_size(size).unwrap();
+            window.center().unwrap();
 
             #[cfg(desktop)]
             {
--- src/keybinds.ts
+++ src/keybinds.ts
@@ -5,16 +5,20 @@ import {
   unregister,
 } from "@tauri-apps/plugin-global-shortcut";
 
+const toggleKeybind = "CommandOrControl+Shift+E";
+
 export async function registerKeybinds() {
   // reregister if already set
-  if (await isRegistered("CommandOrControl+Shift+E")) {
-    unregister("CommandOrControl+Shift+E");
+  if (await isRegistered(toggleKeybind)) {
+    unregister(toggleKeybind);
   }
 
-  await register("CommandOrControl+Shift+E", async (event) => {
+  await register(toggleKeybind, async (event) => {
     if (event.state === "Pressed") {
       console.log("Shortcut triggered");
       if (await getCurrent().isVisible()) {
+        // look at this more https://stackoverflow.com/questions/6078799/minimize-restore-windows-programmatically-skipping-the-animation-effect
+        // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
         await getCurrent().hide();
       } else {
         await getCurrent().show();
